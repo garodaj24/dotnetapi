ccffc29d263e691c72f70891bf5ac492cbbe699a
Garod <garod.ajemian@bridgefield.de>
2024-09-09 15:18:01 +0400

    added contrp;;er, dto and mapper

---
+++ Mappers/StockMappers.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnetapi.Dtos.Stock;
+using dotnetapi.Models;
+
+namespace dotnetapi.Mappers
+{
+    public static class StockMappers
+    {
+        public static StockDto ToStockDto(this Stock stockModel)
+        {
+            return new StockDto
+            {
+                Id = stockModel.Id,
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Purchase = stockModel.Purchase,
+                LastDiv = stockModel.LastDiv,
+                Industry = stockModel.Industry,
+                MarketCap = stockModel.MarketCap
+            };
+        }
+    }
+}
