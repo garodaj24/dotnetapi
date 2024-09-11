21684d10df1988752288c4f24bb84a4bcce032be
Garod <garod.ajemian@bridgefield.de>
2024-09-11 17:36:41 +0400

    added create user functionality

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnetapi.Dtos.Account;
+using dotnetapi.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetapi.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var user = new AppUser
+                {
+                    UserName = registerDto.Username,
+                    Email = registerDto.Email
+                };
+
+                #pragma warning disable CS8604 // Possible null reference argument.
+                var createdUser = await _userManager.CreateAsync(user, registerDto.Password);
+                #pragma warning restore CS8604 // Possible null reference argument.
+
+                if (createdUser.Succeeded)
+                {
+                    var roleResult = await _userManager.AddToRoleAsync(user, "User");
+                    if (roleResult.Succeeded)
+                    {
+                        return Ok("User created successfully");
+                    }
+                    else
+                    {
+                        return BadRequest(roleResult.Errors);
+                    }
+                }
+                else
+                {
+                    return BadRequest(createdUser.Errors);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
