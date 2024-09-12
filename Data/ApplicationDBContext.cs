3709676fa3df382f9c74b7aeb6cd0ca2cb19175b
Garod <garod.ajemian@bridgefield.de>
2024-09-12 13:00:30 +0400

    added portfolios many to many relationship

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -18,9 +18,22 @@ namespace dotnetapi.Data
 
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Portfolio>().HasKey(p => new { p.AppUserId, p.StockId });
+
+            modelBuilder.Entity<Portfolio>()
+                .HasOne(p => p.AppUser)
+                .WithMany(u => u.Portfolios)
+                .HasForeignKey(p => p.AppUserId);
+
+            modelBuilder.Entity<Portfolio>()
+                .HasOne(p => p.Stock)
+                .WithMany(s => s.Portfolios)
+                .HasForeignKey(p => p.StockId);
 
             List<IdentityRole> roles = new List<IdentityRole>
             {
--- Models/Stock.cs
+++ Models/Stock.cs
@@ -19,5 +19,6 @@ namespace dotnetapi.Models
         public long MarketCap { get; set; }
 
         public List<Comment> Comments { get; set; } = new List<Comment>();
+        public List<Portfolio> Portfolios { get; set; } = new List<Portfolio>();
     }
 }
